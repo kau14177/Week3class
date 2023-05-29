5add42a8ec07a92ccd23d81d2120bda749706c67
kau14177 <kau14177@sheridancollege.ca>
2023-05-29 12:28:40 -0600

    Version 1

---
+++ src/week3/Week3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week3;
+
+/** Local rep - steps -1. initialize versioning, .gitconfig, .gitignore, .commit, 
+ *
+ * @author Gagandeep Kaur
+ */
+public class Week3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        
+        
+    }
+    
+}
